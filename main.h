f52801072d5f3881eb142a2a392cc0900a4385cf
Linf0rd <linfordsnagger@gmx.com>
2023-06-28 03:15:53 +0200

    Add main.h header file
    Create the main.h header file to define the function prototypes and include necessary header files for the implementation of the printf function.

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(va_list args);
+
+
+#endif /* MAIN_H */
